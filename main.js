12c4044775e716b9cd6f841b2d2c3e8ec0adf99e
Euston <raja.r184@gmail.com>
2016-07-11 17:47:58 +0100

    better organisation

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-var roleHarvester = require('role.economy');
-var roleUpgrader = require('role.economy');
-var roleBuilder = require('role.economy');
-var roomMonitor = require('role.defence');
-var roleTower = require('role.defence');
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var roleTower = require('role.tower');
 
 module.exports.loop = function () {
-    for(var name in Game.rooms) {
-        roomMonitor.run(Game.rooms[name]);
+
+    for (let name in Memory.creeps) {
+        // and checking if the creep is still alive
+        if (Game.creeps[name] == undefined) {
+            // if not, delete the memory entry
+            delete Memory.creeps[name];
+        }
+    }
+
+    for(let tower in Game.spawns.NorthGreenwich.room.find(
+                FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}})) {
+        roleTower.run(tower);
     }
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
@@ -18,6 +27,18 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
+        }
+    }
+    var minreqs = {
+        'harvester': 4,
+        'upgrader': 0,
+        'builder': 0
+    }
+
+    for(let key in minreqs) {
+        var nums = _.sum(Game.creeps, (c) => c.memory.role == key);
+        if(nums < minreqs[key]) {
+            Game.spawns.NorthGreenwich.createCreep([WORK, CARRY, MOVE], undefined, {role: key})
         }
     }
 }
---
+++ role.tower.js
@@ -0,0 +1,11 @@
+var roleTower = {
+    run: function(tower) {
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+}
+
+
+module.exports = roleTower;
